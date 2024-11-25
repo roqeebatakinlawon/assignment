ac6466608d5cd8db2964823b235d8b8d517c938e
roqeebatakinlawon <roqeebatakinlawon007@gmail.com>
2024-11-25 18:25:14 +0100

    Changes to be committed: 	new file:   asset/Ellipse 1-min.png 	new file:   asset/Footer logo 2.0.png    new file:   asset/IMG_E1424.JPG 	new file:   asset/Scissors Logo-min.png 	new file:   asset/activity.png 	new file:   asset/background-img.jpg 	new file:   asset/edit.png 	new file:   asset/grid.png 	new file:   asset/link-2.png 	new file:   asset/magic wand.png 	new file:   asset/shorter link-min.png 	new file:   asset/wppusher-logo copy.png 	modified:   assignment5.css 	modified:   assignment5.html 	modified:   assignment6.css 	modified:   assignment6.html 	modified:   index.html 	new file:   queries.css 	new file:   script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Function to Show Side-Bar
+function showSidebar() {
+  const sidebar = document.querySelector(".side-bar");
+  sidebar.style.display = "flex";
+}
+
+// Function to Hide Side-Bar
+function hideSidebar() {
+  const sidebar = document.querySelector(".side-bar");
+  sidebar.style.display = "none";
+}
